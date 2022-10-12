db056c767bcb1a0b6f2799e07f4fbc239677fbc8
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-12 14:08:50 -0700

    edits and homepage route

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Giver, Taker, Post, Item } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all posts and JOIN with item and giver data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: Giver,
+          attributes: ['username'],
+        },
+        {
+          model: Item,
+          attributes: ['name', 'description']
+        }
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// // Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in giver based on the session ID
+//     const giverData = await Giver.findByPk(req.session.giver_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Post }],
+//     });
+
+//     const giver = giverData.get({ plain: true });
+
+//     res.render('profile', {
+//       ...giver,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/login', (req, res) => {
+//   // If the giver is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/homepage');
+//     return;
+//   }
+
+//   res.render('loginpage');
+// });
+
+module.exports = router;
