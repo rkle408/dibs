66e24ab238646e12d57bb2e4861fae2c245de762
Rosebud <rkle408@gmail.com>
2022-10-13 11:25:36 -0700

    may remove takerRoute as it is redundant, commented out for now

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const takerRoutes = require('./takerRoute');
+// const takerRoutes = require('./takerRoute');
 const giverRoutes = require('./giverRoute');
 const itemRoutes = require('./itemRoute');
 const postRoutes = require('./postRoute');
 
-router.use('/taker', takerRoutes);
+// router.use('/taker', takerRoutes);
 router.use('/giver', giverRoutes);
 router.use('/item', itemRoutes);
 router.use('/post', postRoutes);
