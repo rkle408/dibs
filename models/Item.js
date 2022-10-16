68121cf3c16dc14e6cc9241273b03fc4dea4aa0b
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-16 10:23:40 -0700

    picked-up button deletes post

--- models/Item.js
+++ models/Item.js
@@ -10,7 +10,7 @@ Item.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true
+            
         },
         name: {
             type: DataTypes.STRING,
--- public/js/newItem.js
+++ public/js/newItem.js
@@ -9,6 +9,13 @@ const newItemHandler = async (event) => {
  
   
 
+  const response = await fetch('/api/item', {
+    method: 'POST',
+    body: JSON.stringify({ name: itemName, description: itemDescription }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  console.log(response);
+
   async function fetchItems() {
     const response = await fetch('/api/item');
     if (!response.ok) {
@@ -16,11 +23,11 @@ const newItemHandler = async (event) => {
       throw new Error(message);
     }
     const items = await response.json();
-    console.log(items)
-    return items.length;
+    console.log(items[items.length-1].id)
+    return items[items.length-1].id;
   }
  var itemCount = await fetchItems();
- itemCount++;
+
   // console.log("This is the number of items")
   // fetchItems().then(function (result){
   //   console.log(result)
@@ -32,12 +39,6 @@ const newItemHandler = async (event) => {
   console.log(itemImage.files[0])
   let formData = new FormData();
   formData.append('file',itemImage.files[0]);
-  const response = await fetch('/api/item', {
-    method: 'POST',
-    body: JSON.stringify({ name: itemName, description: itemDescription }),
-    headers: { 'Content-Type': 'application/json' },
-  });
-  console.log(response);
   const response2 = await fetch(`/upload/${itemCount}`, {
     method: 'POST',
     body: formData
@@ -46,7 +47,7 @@ const newItemHandler = async (event) => {
   console.log("hit second response")
 
   // This will allow window to automatically refresh when we post new item
-  window.location.reload();
+  // window.location.reload();
 }
 
 document.getElementById('new-post-submit')
